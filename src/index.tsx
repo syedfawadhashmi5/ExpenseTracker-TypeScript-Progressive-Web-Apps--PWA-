dd54140c3ef4b50379f81b275d6edf8d99dad878
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-11-02 13:21:03 -0800

    update firebase

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import swDev from './service/ServiceRegister'
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +13,10 @@ ReactDOM.render(
 );
 
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
+
+
+swDev();
